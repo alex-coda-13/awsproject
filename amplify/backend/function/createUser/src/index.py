2f5318745b1a234a26f529379520e17f64652b9f
alexandre codaccioni <alexandre.codaccioni@gmail.com>
2023-04-18 10:11:31 +0200

    createUser

---
+++ amplify/backend/function/createUser/src/index.py
@@ -0,0 +1,60 @@
+import json
+import os
+import uuid
+
+import boto3
+
+from boto3.dynamodb.conditions import Key, Attr
+
+dbb = boto3.resource('dynamodb')
+
+def handler(event, context):
+  response = {}
+  try:
+    httpMethod = event.get('httpMethod')
+    if not httpMethod or httpMethod != 'POST':
+      raise ValueError('Wrong Rest Method')
+
+    # body = json.loads(event.get('body'))
+    body = event.get('body')
+
+    tableName = os.environ['STORAGE_USERS_NAME']
+    table = dbb.Table(tableName)
+
+    response['body'] = json.dumps(check_user(body=body, table=table))
+    response['statusCode'] = 200
+
+  except Exception as err:
+    response['statusCode'] = 400
+    response['body'] = { 'error': str(err) }
+  
+  return response
+    
+
+def check_user(body, table):
+  inputs = ['email', 'firstname', 'lastname', 'age']
+
+  for elm in inputs:
+    if elm not in body: 
+      raise ValueError(f'Missing input: {elm}')
+  
+  item = table.query(
+    IndexName="emailIndex",
+    KeyConditionExpression=Key('email').eq(body['email']),
+    ProjectionExpression="id, firstname"
+  )
+
+  print(item)
+
+  if len(item['Items']) <= 0:
+
+    body['id'] = str(uuid.uuid4())
+    table.put_item(
+      Item=body
+    )
+    return { 'user_id': body['id'] }
+  else:
+    return { 'user_id': item['Items'][0]['id'] }
+
+
+
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "users": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "createUser": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "users": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
