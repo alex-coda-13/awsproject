a94983e150883011bb6b1648631d487785534d29
alexandre codaccioni <alexandre.codaccioni@gmail.com>
2023-04-18 11:31:37 +0200

    getUser

--- amplify/backend/function/getUser/src/index.py
+++ amplify/backend/function/getUser/src/index.py
@@ -1,15 +1,42 @@
 import json
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+dbb = boto3.resource('dynamodb')
 
 def handler(event, context):
-  print('received event:')
-  print(event)
+  response = {}
+
+  try:
+    httpMethod = event.get('httpMethod')
+    if not httpMethod or httpMethod != 'GET':
+      raise ValueError('Wrong Rest Method')
   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    params = event.get('queryStringParameters') or {}
+    user_id = params.get('id')
+
+    if not user_id:
+      raise ValueError('Missing user id')
+
+    tableName = os.environ['STORAGE_USERS_NAME']
+    table = dbb.Table(tableName)
+
+    response['body'] = json.dumps(get_user(user_id=user_id, table=table))
+    response['statusCode'] = 200
+
+  except (Exception, ClientError) as err:
+    print(err)
+    response['statusCode'] = 400
+    response['body'] = { 'error': str(err) }
+  
+  return response
+
+def get_user(user_id, table):
+  user = table.get_item(
+    Key={ 'id': user_id },
+    ProjectionExpression="firstname, lastname, email, age"
+  )
+
+  return { 'user': user.get('Item') }
